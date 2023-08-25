8dbe9af30bc10e31fbf2abf232540c0fe09c69ba
LeonardoAK <leonardo.acostak@gmail.com>
2023-08-25 11:50:49 -0300

    Commit desde ofi 25-08

--- src/main/java/com/adv/videodb/repositorios/CarpetaRepositorio.java
+++ src/main/java/com/adv/videodb/repositorios/CarpetaRepositorio.java
@@ -2,6 +2,7 @@
 package com.adv.videodb.repositorios;
 
 import com.adv.videodb.entidades.Carpeta;
+import java.util.Date;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,9 @@ public interface CarpetaRepositorio extends JpaRepository<Carpeta, String>{
     @Query("SELECT c FROM carpeta c WHERE c.nombre = :nombre")
     public List<Carpeta> buscarPorNombre(@Param("nombre") String nombre);
     
+    @Query("SELECT c FROM carpeta c WHERE c.fecha = :fecha")
+    public List<Carpeta> buscarPorFecha(@Param("fecha") Date fecha);
+    
+    @Query("SELECT c FROM carpeta c WHERE c.tdc = :tdc")
+    public List<Carpeta> buscarPorTdc(@Param("tdc") String tdc);
 }
